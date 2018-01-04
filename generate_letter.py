87a6008bdf5687a3140d3d29bee57aad2761669b
Rakesh Ravi Shankar <kcrakeshravi@gmail.com>
2018-01-04 11:46:20 +0530

    Fetch from address, fixed key retrieval

--- generate_letter.py
+++ generate_letter.py
@@ -4,8 +4,26 @@ import json
 
 # Load api keys from local config file
 keys = json.load(open('keys.json'))
-lob.api_key = keys.lob_api_key
-civic_api_key = keys.civic_api_key
+lob.api_key = keys.get('lob_api_key')
+civic_api_key = keys.get('civic_api_key')
+
+
+# Ask user for input args
+def parse_input_args():
+    from_address = {}
+    message_payload = ''
+
+    print 'Enter from address details:'
+    from_address['name'] = raw_input('Enter name: ')
+    from_address['address_line1'] = raw_input('Enter address line 1: ')
+    from_address['address_line2'] = raw_input('Enter address line 2: ')
+    from_address['address_city'] = raw_input('Enter city: ')
+    from_address['address_state'] = raw_input('Enter state: ')
+    from_address['address_zip'] = raw_input('Enter zip: ')
+    from_address['address_country'] = raw_input('Enter country: ')
+    message_payload = raw_input('Enter your message to the legislator: ')
+
+    return from_address, message_payload
 
 
 # Pull legislator info using the Google Civic api
@@ -22,7 +40,7 @@ def get_legislator_info(address):
 
 
 # Compose and send a letter using the Lob api
-def send_letter():
+def send_letter(from_address, message_payload):
     letter = lob.Letter.create(
       description = 'Letter to legislator',
       to_address = {
@@ -34,20 +52,15 @@ def send_letter():
           'address_zip': '94107',
           'address_country': 'US'
       },
-      from_address = {
-          'name': 'Leore Avidar',
-          'address_line1': '185 Berry St',
-          'address_line2': '# 6100',
-          'address_city': 'San Francisco',
-          'address_state': 'CA',
-          'address_zip': '94107',
-          'address_country': 'US'
-      },
+      from_address = from_address,
       file = 'tmpl_5c64c3094c0e11c',
       merge_variables = {
-        'message': 'Harry'
+        'message': message_payload
       },
       color = True
     )
     print '\n\nLetter Generation Response:'
     print letter
+
+
+
